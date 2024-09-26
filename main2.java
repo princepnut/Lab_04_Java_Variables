36e0364b4b99ec0f54c5cbad8a2a2347523d2dce
princepnut <kaijavaughn@gmail.com>
2024-09-26 13:19:01 -0400

    Add files via upload
    Completed Part 1

---
+++ main2.java
@@ -0,0 +1,38 @@
+public class main2{
+    public static void main(String[] args) {
+        int intOperandA = 1;
+        int intOperandB = 2;
+        int intSum;
+        int intProduct;
+        int intDifference;
+        int intQuotient;
+        int intModulo;
+        double doubleOperandA = 3.50;
+        double doubleOperandB = 4.50;
+        double doubleSum;
+        double doubleProduct;
+        double doubleDifference;
+        double doubleQuotient ;
+        intSum = intOperandB + intOperandA;
+        intProduct = intOperandB * intOperandA;
+        intDifference = intOperandB - intOperandA;
+        intQuotient = intOperandB / intOperandA;
+        intModulo = intOperandA % intOperandB;
+        doubleSum = doubleOperandB + doubleOperandA;
+        doubleProduct = doubleOperandB * doubleOperandA;
+        doubleDifference = doubleOperandB - doubleOperandA;
+        doubleQuotient = doubleOperandB / doubleOperandA;
+        System.out.println("The sum using int of " + intOperandB + " and " + intOperandA + " is " + intSum);
+        System.out.println("The product using int of " + intOperandB + " and " + intOperandA + " is " + intProduct);
+        System.out.println("The difference using int of " + intOperandB + " and " + intOperandA + " is " + intDifference);
+        System.out.println("The quotient using int of " + intOperandB + " and " + intOperandA + " is " + intQuotient);
+        System.out.println("The modulo using int of " + intOperandA + " and " + intOperandB + " is " + intModulo);
+        System.out.println("The sum using int of " + doubleOperandB + " and " + doubleOperandA + " is " + doubleSum);
+        System.out.println("The product using int of " + doubleOperandB + " and " + doubleOperandA + " is " + doubleProduct);
+        System.out.println("The difference using int of " + doubleOperandB + " and " + doubleOperandA + " is " + doubleDifference);
+        System.out.println("The quotient using int of " + doubleOperandB + " and " + doubleOperandA + " is " + doubleQuotient);
+
+    }
+}
+
+
---
+++ main3.java
@@ -0,0 +1,19 @@
+public class main3 {
+    public static void main(String[] args) {
+        int numberKids = 0;
+        String rainingQ = "No";
+        double priceGas = 3.070;
+        int favNumber = 4;
+        double shoeSize = 6.5;
+        int birthMonth = 8;
+        String birthMonth2 = " / August";
+        String fullName = "Jane Doe";
+        System.out.println("The number of kids in my family is: " + numberKids);
+        System.out.println("Is it raining?: " + rainingQ);
+        System.out.println("The price of gas is: " + priceGas);
+        System.out.println("My favorite number is: " + favNumber);
+        System.out.println("My shoe size is: " + shoeSize);
+        System.out.println("My birth month is: " + birthMonth + birthMonth2);
+        System.out.println("My full name is: " + fullName);
+    }
+}
